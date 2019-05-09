7052dd89efd325169ac98ce40a5f7bb5ed661d0a
yoyoyo <594372929@qq.com>
2019-05-09 10:30:33 +0800

    修改 userLayout 样式

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -2,53 +2,21 @@ import React, { Fragment } from 'react';
 import Link from 'umi/link';
 import { Icon } from 'antd';
 import GlobalFooter from '@/components/GlobalFooter';
-// import SelectLang from '@/components/SelectLang';
 import styles from './UserLayout.less';
-import logo from '../assets/logo.svg';
-
-const links = [
-  {
-    key: 'help',
-    title:'',
-    href: '',
-  },
-  {
-    key: 'privacy',
-    title:'',
-    href: '',
-  },
-  {
-    key: 'terms',
-    title:'',
-    href: '',
-  },
-];
 
 const copyright = (
   <Fragment>
-    Copyright <Icon type="copyright" /> 2018 蚂蚁金服体验技术部出品
+    Copyright <Icon type="copyright" /> 深圳前海翼联有限公司
   </Fragment>
 );
 
 const UserLayout = ({ children }) => (
   // @TODO <DocumentTitle title={this.getPageTitle()}>
   <div className={styles.container}>
-    <div className={styles.lang}>
-      {/*<SelectLang />*/}
-    </div>
     <div className={styles.content}>
-      <div className={styles.top}>
-        <div className={styles.header}>
-          <Link to="/">
-            <img alt="logo" className={styles.logo} src={logo} />
-            <span className={styles.title}>Ant Design</span>
-          </Link>
-        </div>
-        <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
-      </div>
       {children}
     </div>
-    <GlobalFooter links={links} copyright={copyright} />
+    <GlobalFooter copyright={copyright} />
   </div>
 );
 
